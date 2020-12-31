2a17d9b52ff81d4c65a84a02882d0b9cb0474a6c
Amogh Dhavalikar <amoghdhavalikar@gmail.com>
2020-12-31 12:34:07 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,24 @@
+a = input ("Enter any numeric value(a): ")
+b = input ("Enter one more numeric value(b): ")
+
+c= int(a) + int(b)
+
+if a > b :
+    print ("a(", a ,") is greater than b(", b ,")")
+elif a == b :
+    print ("a(",a,") is equal to b(", b ,")")
+elif b > a:
+    print ("b(", b ,") is greater than a(", a ,")")
+
+
+
+print ("Value A + Value B= ",c)
+
+
+print("")
+print("")
+print("")
+print("")
+input ("Press enter to exit")
+
+
---
+++ python2.py
@@ -0,0 +1,32 @@
+a = input ("How many calculations do you want to perform? ")
+
+i = 0
+
+while (i < int(a)):
+    number = int(input("enter a number: "))
+    output1 = number ** 0.5
+    output2 = number * number
+    print("Square root: ", output1)
+    print("Square: ", output2)
+    print("Cube Root: ", number ** (1/3))
+    print("Cube: ", number ** 3)
+
+
+    input ("");
+    i+=1
+
+import math
+
+#b = input ("One more number please! ")
+
+#print (math.int(a))
+
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+
+input ("Press enter to exit ")
